477ae5be18b2571c8bc94e4ab3294c8174c3edb4
Elizabeth Munoz <munoz_714@live.com>
2020-06-21 17:32:21 -0700

    employee setState new data

--- emdir/src/App.js
+++ emdir/src/App.js
@@ -1,32 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchForm from "./components/SearchForm";
 import API from "./utils/API";
 
 function App() {
-  const [] = useState;
+  const [state, setState] = useState({
+    employees: [],
+  });
 
   // When the component mounts, get a list of all available base breeds and update this.state.breeds
-  componentDidMount(){
+  useEffect(() => {
     API.getEmployee()
-      .then(res => this.setState({ employee: res.data }))
-      .catch(err => console.log(err));
-  }
-
-  handleInputChange = event => {
-
-  };
-
-  handleFormSubmit = event => {
-    event.preventDefault();
-    API.getEmployee(this.state.search)
-      .then(res => {
-        if (res.data.status === "error") {
-          throw new Error(res.data.message);
+      .then((res) => {
+        console.log("this is our dataaa", res.data.results);
+        var newEmps = [];
+        var data = res.data.results;
+        for (var i = 0; i < data.length; i++) {
+          var emp = {
+            name: data[i].name.first + " " + data[i].name.last,
+            email: data[i].email,
+            phone: data[i].phone,
+            pic: data[i].picture.thumbnail,
+          };
+          newEmps.push(emp);
         }
-        this.setState({ results: res.data. });
+        console.log("new data", newEmps);
+        setState({ ...state, employees: newEmps });
+
+        //this.setState({ employee: res.results })
       })
-      .catch(err => this.setState({ error: err.message }));
-  };
+      .catch((err) => console.log(err));
+  }, []);
+
+  // handleInputChange = event => {
+
+  // };
+
+  // handleFormSubmit = event => {
+  //   event.preventDefault();
+  //   API.getEmployee(this.state.search)
+  //     .then(res => {
+  //       if (res.data.status === "error") {
+  //         throw new Error(res.data.message);
+  //       }
+  //       this.setState({ results: res.data. });
+  //     })
+  //     .catch(err => this.setState({ error: err.message }));
+  // };
+  console.log(state);
   return (
     <>
       <div className="App">
--- emdir/src/utils/API.js
+++ emdir/src/utils/API.js
@@ -4,6 +4,6 @@ import axios from "axios";
 
 export default {
   getEmployee: function () {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us");
+    return axios.get("https://randomuser.me/api/?results=10&nat=us");
   },
 };
